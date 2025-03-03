e498782e8f6c0558d40dbf9499a1d4190be0f927
dianakhnizova <khnizovad@gmail.com>
2025-03-03 21:10:36 +0600

    feat: complete task - sellTickets

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -148,8 +148,32 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let cash25 = 0;
+  let cash50 = 0;
+
+  return queue.reduce((canSell, bill) => {
+    if (!canSell) return false;
+
+    if (bill === 25) {
+      cash25 += 1;
+    } else if (bill === 50) {
+      if (cash25 === 0) return false;
+      cash25 -= 1;
+      cash50 += 1;
+    } else if (bill === 100) {
+      if (cash50 > 0 && cash25 > 0) {
+        cash50 -= 1;
+        cash25 -= 1;
+      } else if (cash25 >= 3) {
+        cash25 -= 3;
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  }, true);
 }
 
 /**
